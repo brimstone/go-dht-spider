1f10447dc423c6b919284bface26c8244e081d5d
Matt Robinson <brimstone@the.narro.ws>
2016-08-09 20:22:17 -0400

    Use newer example

--- spider.go
+++ spider.go
@@ -2,32 +2,68 @@ package main
 
 import (
 	"encoding/hex"
+	"encoding/json"
+	"fmt"
 	"log"
 
 	"github.com/shiyanhui/dht"
 )
 
+type File struct {
+	Path   []interface{} `json:"path"`
+	Length int           `json:"length"`
+}
+type BitTorrent struct {
+	InfoHash string `json:"infohash"`
+	Name     string `json:"name"`
+	Files    []File `json:"files,omitempty"`
+	Length   int    `json:"length,omitempty"`
+}
+
 func main() {
-	downloader := dht.NewWire()
+	w := dht.NewWire()
 	go func() {
-		// once we got the request result
-		for resp := range downloader.Response() {
-			log.Println(string(resp.InfoHash), string(resp.MetadataInfo))
+		for resp := range w.Response() {
+			metadata, err := dht.Decode(resp.MetadataInfo)
+			if err != nil {
+				continue
+			}
+			info := metadata.(map[string]interface{})
+			if _, ok := info["name"]; !ok {
+				continue
+			}
+			bt := BitTorrent{
+				InfoHash: hex.EncodeToString(resp.InfoHash),
+				Name:     info["name"].(string),
+			}
+			if v, ok := info["files"]; ok {
+				files := v.([]interface{})
+				bt.Files = make([]File, len(files))
+				for i, item := range files {
+					file := item.(map[string]interface{})
+					bt.Files[i] = File{
+						Path:   file["path"].([]interface{}),
+						Length: file["length"].(int),
+					}
+				}
+			} else if _, ok := info["length"]; ok {
+				bt.Length = info["length"].(int)
+			}
+			data, err := json.Marshal(bt)
+			if err == nil {
+				fmt.Printf("%s\n\n", data)
+			}
 		}
 	}()
-	go downloader.Run()
+	go w.Run()
 
 	config := dht.NewCrawlConfig()
 	log.Println("Spider configured")
-	/*
-		config.OnGetPeers = func(infohash, ip string, port int) {
-			log.Println("OnGetPeers:", hex.EncodeToString([]byte(infohash)), ip, port)
-		}
-	*/
+
 	config.OnAnnouncePeer = func(infoHash, ip string, port int) {
 		log.Println("Attempting to download", hex.EncodeToString([]byte(infoHash)), "from", ip, port)
 		// request to download the metadata info
-		downloader.Request([]byte(infoHash), ip, port)
+		w.Request([]byte(infoHash), ip, port)
 	}
 	d := dht.New(config)
 
