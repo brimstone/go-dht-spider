0d7cc732c729115def7bbf20112e3f9dddea9f76
Matt Robinson <brimstone@the.narro.ws>
2016-08-10 21:26:52 -0400

    Update code

--- spider.go
+++ spider.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"os"
+	"time"
 
 	"github.com/shiyanhui/dht"
 )
@@ -14,14 +16,31 @@ type File struct {
 	Length int           `json:"length"`
 }
 type BitTorrent struct {
-	InfoHash string `json:"infohash"`
-	Name     string `json:"name"`
-	Files    []File `json:"files,omitempty"`
-	Length   int    `json:"length,omitempty"`
+	Timestamp int64  `json:"@timestamp"`
+	InfoHash  string `json:"infohash"`
+	Name      string `json:"name"`
+	Files     []File `json:"files,omitempty"`
+	Length    int    `json:"length,omitempty"`
+}
+
+type Peer struct {
+	Timestamp int64  `json:"@timestamp"`
+	InfoHash  string `json:"infohash"`
+	Address   string `json:"address"`
+	Port      int    `json:"port"`
+}
+
+func Nowish() string {
+	return time.Now().UTC().Format("20060102")
 }
 
 func main() {
 	w := dht.NewWire()
+
+	es := "http://" + os.Getenv("ELASTICSEARCH_PORT_9200_TCP_ADDR") + ":" + os.Getenv("ELASTICSEARCH_PORT_9200_TCP_PORT")
+	DeleteTemplate(es)
+	SetTemplate(es)
+
 	go func() {
 		for resp := range w.Response() {
 			metadata, err := dht.Decode(resp.MetadataInfo)
@@ -53,6 +72,12 @@ func main() {
 			if err == nil {
 				fmt.Printf("%s\n\n", data)
 			}
+
+			err = Index(es+"/dht-"+Nowish()+"/infohash/"+string(bt.InfoHash), data)
+			if err != nil {
+				log.Println("Error saving to ES:", err)
+			}
+
 		}
 	}()
 	go w.Run()
@@ -62,9 +87,36 @@ func main() {
 
 	config.OnAnnouncePeer = func(infoHash, ip string, port int) {
 		log.Println("Attempting to download", hex.EncodeToString([]byte(infoHash)), "from", ip, port)
+
+		peer, _ := json.Marshal(Peer{
+			Timestamp: time.Now().Unix(),
+			InfoHash:  hex.EncodeToString([]byte(infoHash)),
+			Address:   ip,
+			Port:      port,
+		})
+
+		err := Index(es+"/dht-"+Nowish()+"/announce/", peer)
+		if err != nil {
+			log.Println("Error saving to ES:", err)
+		}
 		// request to download the metadata info
 		w.Request([]byte(infoHash), ip, port)
 	}
+
+	config.OnGetPeers = func(infoHash, ip string, port int) {
+		peer, _ := json.Marshal(Peer{
+			Timestamp: time.Now().Unix(),
+			InfoHash:  hex.EncodeToString([]byte(infoHash)),
+			Address:   ip,
+			Port:      port,
+		})
+
+		err := Index(es+"/dht-"+Nowish()+"/request/", peer)
+		if err != nil {
+			log.Println("Error saving to ES:", err)
+		}
+	}
+
 	d := dht.New(config)
 
 	log.Println("Spider started")
